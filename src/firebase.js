09e1c32be07301512a57d3714519df2ed4474720
unknown <anton13jeejo@gmail.com>
2020-10-26 00:49:52 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdCso66Pnh_zeovHd1excZ8W0aM5Fqb6g",
+  authDomain: "airbnb-clone-70095.firebaseapp.com",
+  databaseURL: "https://airbnb-clone-70095.firebaseio.com",
+  projectId: "airbnb-clone-70095",
+  storageBucket: "airbnb-clone-70095.appspot.com",
+  messagingSenderId: "825636869658",
+  appId: "1:825636869658:web:fb533213a4ce45e58ae3a3",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
