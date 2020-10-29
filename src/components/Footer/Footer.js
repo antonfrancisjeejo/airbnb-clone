38a3f87849dfdcc28bb6e8914440faa331e94cde
unknown <anton13jeejo@gmail.com>
2020-10-30 00:35:59 +0530

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>&copy; 2020 Airbnb clone! No rights reserved</p>
+      <p>Privacy • Terms • Sitemap • Company Details</p>
+    </div>
+  );
+};
+
+export default Footer;
